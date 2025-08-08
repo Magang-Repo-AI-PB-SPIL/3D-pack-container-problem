f4dd185e957e037284518da341881d0f9e4d8621
M.Farhan Nur Rohman <52419427+FarhanNurrohman@users.noreply.github.com>
2025-08-08 15:30:17 +0700

    push

--- solution/combination container pickup.py
+++ solution/combination container pickup.py
@@ -17,7 +17,6 @@ class Item:
 def find_combinations(items, container_size, max_weight):
     container_vol = container_size[0] * container_size[1] * container_size[2]
     valid_combos = []
-    item_name = [item.name for item in items]
 
     max_counts = []
     for item in items:
@@ -28,6 +27,7 @@ def find_combinations(items, container_size, max_weight):
     for counts in product(*(range(m + 1) for m in max_counts)):
         total_vol = sum(item.volume * count for item, count in zip(items, counts))
         total_weight = sum(item.weight * count for item, count in zip(items, counts))
+        print(f"counts={counts}, total_vol={total_vol}, total_weight={total_weight}")
 
         if total_vol <= container_vol and total_weight <= max_weight:
             valid_combos.append((counts, total_vol, total_weight))
@@ -50,7 +50,7 @@ if __name__ == "__main__":
 
     valid_combos = find_combinations(items, (D, L, H), MAX_WEIGHT)
     print(f"Total kombinasi valid: {len(valid_combos)}")
-    for combos, vol, weight in valid_combos[-10:]:  # tampilkan 10 contoh
+    for combos, vol, weight in valid_combos[:10]:  # tampilkan 10 contoh
         for combo, item in zip(combos, items):
             print(f"barang {item.name} dengan jumlah {combo}")
         print(f"Volume={vol:.2f}", f"Weight={weight:.2f}")
